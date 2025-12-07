81aa33c7aabaf99a8a4dcb68112d57188adec3f4
OmarVD-code <omarvillarreal2000@gmail.com>
2025-12-06 22:08:21 -0500

    first template of welcome message with Las Vegas style

--- src/components/Hero/Welcome.tsx
+++ src/components/Hero/Welcome.tsx
@@ -1,25 +1,25 @@
 import "./styles/Welcome.css";
 
 export default function Welcome() {
-    const letters = ["W", "E", "L", "C", "O", "M", "E"];
-
     return (
         <section id="wallpaper_" className="wallpaper">
-            <div className="vegas-sign">
-                <div className="vegas-sign-inner">
-                    <div className="vegas-sign-welcome">
-                        {letters.map(l => (
-                            <span key={l} className="vegas-sign-circle">
-                                {l}
-                            </span>
-                        ))}
-                    </div>
-
-                    <div className="vegas-sign-to">TO</div>
-                    <div className="vegas-sign-fabulous">My portfolio</div>
-                    <div className="vegas-sign-city">OMAR VILLARREAL</div>
-                    <div className="vegas-sign-state">SOFTWARE DEVELOPER</div>
+            <div className="sign"></div>
+            <div className="text">
+                <div className="welcome">
+                    <span>W</span>
+                    <span>E</span>
+                    <span>L</span>
+                    <span>C</span>
+                    <span>O</span>
+                    <span>M</span>
+                    <span>E</span>
                 </div>
+                <div className="to">
+                    <span>TO</span>
+                </div>
+                <div className="fab">My Portfolio</div>
+                <div className="las">OMAR VILLARREAL</div>
+                <div className="nv">SOFTWARE DEVELOPER</div>
             </div>
         </section>
     )
