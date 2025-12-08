2b41a1b03da40a5e45353bd4d7676b434cc85d4e
OmarVD-code <omarvillarreal2000@gmail.com>
2025-12-07 19:34:27 -0500

    add poker table style to contact form

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,21 +1,27 @@
 export default function ContactForm() {
     return (
-        <form action="https://formsubmit.co/5c3c98f506c3d6cbdb5d4aea63046ef2" method="POST">
-            <p>
-                <label htmlFor="name">Fullname</label>
-                <input type="text" name="name" id="name" required />
-            </p>
-            <p>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" name="email" id="email" required />
-            </p>
-            <p>
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows={3} required />
-            </p>
-            <p>
-                <button type="submit">Send <i className="fas fa-paper-plane" /></button>
-            </p>
-        </form>
+        <div>
+            <div className="poker-table">
+                <div className="table-inner">
+                    <form action="https://formsubmit.co/5c3c98f506c3d6cbdb5d4aea63046ef2" method="POST">
+                        <p>
+                            <label htmlFor="name">Fullname</label>
+                            <input type="text" name="name" id="name" required />
+                        </p>
+                        <p>
+                            <label htmlFor="email">Email Address</label>
+                            <input type="email" name="email" id="email" required />
+                        </p>
+                        <p>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" rows={3} required />
+                        </p>
+                        <p className="contact-form-actions">
+                            <button type="submit">Send <i className="fas fa-paper-plane" /></button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
