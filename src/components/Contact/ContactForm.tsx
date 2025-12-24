ddd7fcef18a3bd6b0d936310101717b6382c0648
OmarVD-code <omarvillarreal2000@gmail.com>
2025-12-24 16:40:29 -0500

    fix some details in contact

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -4,21 +4,21 @@ export default function ContactForm() {
             <div className="poker-table">
                 <div className="table-inner">
                     <form action="https://formsubmit.co/5c3c98f506c3d6cbdb5d4aea63046ef2" method="POST">
-                        <p>
+                        <div className="field">
                             <label htmlFor="name">Fullname</label>
                             <input type="text" name="name" id="name" required />
-                        </p>
-                        <p>
+                        </div>
+                        <div className="field">
                             <label htmlFor="email">Email Address</label>
                             <input type="email" name="email" id="email" required />
-                        </p>
-                        <p>
+                        </div>
+                        <div className="field">
                             <label htmlFor="message">Message</label>
                             <textarea name="message" id="message" rows={3} required />
-                        </p>
-                        <p className="contact-form-actions">
-                            <button type="submit">Send <i className="fas fa-paper-plane" /></button>
-                        </p>
+                        </div>
+                        <div className="contact-form-actions">
+                            <button type="submit">Send</button>
+                        </div>
                     </form>
                 </div>
             </div>
